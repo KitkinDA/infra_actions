d8707be634c1d299230f127d7bb9787fc6227020
Дмитрий Киткин <kitkinda@mail.ru>
2023-05-12 20:49:53 +0300

    test 2

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 app_name = 'infra_app'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/second_page/', views.second_page, name='second_page'),
+    path('second_page/', views.second_page, name='second_page'),
 ]
